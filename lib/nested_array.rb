641dbc73f32f8a593324abff4260c3371ea95824
Jeffrey Platt <jsplatt@gmail.com>
2020-12-08 17:57:04 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,9 +38,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-    matrix_lookup = [
-     sorted_matrix[1][1]
-     ]
+    
      return matrix_lookup[1][1]
   
   # Given any matrix (array of arrays), a row index and a column index, 
